57738c62e5a2053eca401db2933ee4dbb7f59706
rdalejandro22 <alejandro.risco@ucsp.edu.pe>
2016-10-21 09:40:19 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "Punto.h"
 #include "Vector.h"
-/*class PointArray
+class PointArray
 {
     int tam;
     Punto *pt;
@@ -31,7 +31,7 @@
         {
             return tam;
         }
-        void añadirfin(const Punto &p)
+        void aÃ±adirfin(const Punto &p)
         {
             pt = new Punto[size+1];
             for(i=0;i<size;i++)
@@ -42,7 +42,7 @@
             delete pt;
             tam = 0;
         }
-};*/
+};
 int main()
 {
     Punto p1(1,2);
@@ -50,9 +50,9 @@ int main()
     Vector vec1(p1,p2);
     vec1.print();
     vec1.suma(10,10);
-    vec1.print();/*
+    vec1.print();
     Punto lista[5];
     PointArray(lista,5);
-    lista.print();*/
+    lista.print();
 
 }
